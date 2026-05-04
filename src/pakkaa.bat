f96a19d08fcf3afe89f520368544a52ddc158c14
Ruhan12082006 <ruhanghantiwala@gmail.com>
2026-05-04 14:18:11 +0300

    updating the ip and setting up the files

--- src/pakkaa.bat
+++ src/pakkaa.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Java\jdk1.7.0_80\bin\jar" -cfe legoapp.jar app.LegoApp ..\lib\ev3classes.jar .\app .\data .\threads
+"C:\Program Files (x86)\Java\jdk1.7.0_80\bin\jar" -cfe legoapp.jar app.LegoApp .\lib\ev3classes.jar .\src\app .\src\data .\src\threads
